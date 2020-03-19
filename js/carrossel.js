9c9b34b969fb3259f305ee37197d49f0adf1b236
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2020-03-19 17:09:16 -0300

    s

--- js/carrossel.js
+++ js/carrossel.js
@@ -10,9 +10,36 @@ $("#carouselpropostas").owlCarousel({
     items : 1,
     itemsDesktop : [1199,6],
     itemsDesktopSmall : [979,1],
+    itemsMobile : [479,1],
     navigation: false,
     stopOnHover: true,
     pagination: false
+
+});
+
+$("#carouseldeptos").owlCarousel({
+ 
+  autoPlay: 3500, //Set AutoPlay to 3 seconds
+
+  items : 1,
+  itemsDesktopSmall : [979,1],
+  itemsMobile : [479,1],
+  navigation: true,
+  stopOnHover: true,
+  pagination: false
+
+});
+
+$("#carouselmarcas").owlCarousel({
+ 
+  autoPlay: 3500, //Set AutoPlay to 3 seconds
+
+  items : 5,
+  itemsDesktopSmall : [979,5],
+  itemsMobile : [479,2],
+  navigation: true,
+  stopOnHover: true,
+  pagination: false
 
 });
 
