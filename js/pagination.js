b6c74b5434f3a1bfcda2d671f6e72e2d7c4c4764
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2020-04-24 20:43:44 -0300

    a

--- js/pagination.js
+++ js/pagination.js
@@ -1,19 +1,13 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     $(".pages li.previous", "body").html("<img src='/arquivos/seta-esquerda.png'/>")
     $(".pages li.next", "body").html("<img src='/arquivos/seta-direita.png'/>")
 
 
-    //FILTRO MOBILE
+    
 
 
     const mq = window.matchMedia("(max-width: 1000px)");
-    /*if (mq.matches) {
-        $(".navigation-tabs").hide();
-        $( "<div class='visible-xs visible-sm filtro-btn-mobile'><h4>FILTROS</h4></div>" ).insertBefore( ".navigation-tabs" );
-        $(".filtro-btn-mobile").click(()=>{
-            $( ".navigation-tabs" ).toggle( "slow" )
-        })
-    } */
+  
     
 });
 
@@ -28,5 +22,5 @@ $(window).on('hashchange', function(e){
     $(".pages li.previous", "body").html("<img src='/arquivos/seta-esquerda.png'/>");
     $(".pages li.next", "body").html("<img src='/arquivos/seta-direita.png'/>");
    }, 1000)
-
+*/
 
